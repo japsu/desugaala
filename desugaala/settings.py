1d41bb50077794640c4c913e38964b9cd09c0ddb
Santtu Pajukanta <santtu@pajukanta.fi>
2013-01-14 00:25:12 +0200

    fix heroku

--- desugaala/settings.py
+++ desugaala/settings.py
@@ -17,16 +17,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = dj_database_url.config() if 'DATABASE_URL' in os.environ else {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'desugaala.sqlite3',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config() if 'DATABASE_URL' in os.environ else {
+    'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'desugaala.sqlite3',                      # Or path to database file if using sqlite3.
+    'USER': '',                      # Not used with sqlite3.
+    'PASSWORD': '',                  # Not used with sqlite3.
+    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
