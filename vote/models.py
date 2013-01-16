ef4f43c8d8fe509c4823a0217e22f9c7b00a5882
Santtu Pajukanta <santtu@pajukanta.fi>
2013-01-16 21:29:48 +0200

    data fixes

--- vote/models.py
+++ vote/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=64)
-    description = models.CharField(max_length=1024, default='')
+    description = models.CharField(max_length=1024, default='', blank=True)
 
     def __unicode__(self):
       return self.title
