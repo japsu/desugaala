e1d2743f1c27974a00a1172f2ff769904823c2dc
Santtu Pajukanta <santtu@pajukanta.fi>
2013-01-19 17:19:45 +0200

    better status page

--- status/views.py
+++ status/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
-from .models import Watch, Category
+from vote.models import Category, Ballot
+from .models import Watch
 
+@cache_page(5 * 60)
 def status_page(request):
     categories = []
 
     for category in Category.objects.all():
         result = category.evaluate()
-        categories.append((category, result.get('winner'), result.get('tied_winners')))
+        num_votes = category.ballotcategory_set.all().count()
+        categories.append((category, result.get('winner'), result.get('tied_winners'), num_votes))
 
     vars = dict(
+        num_votes=Ballot.objects.all().count(),
         watches=Watch.objects.all(),
         categories=categories
     )
